1b29aed3e4ffe7c4d7f2b517a069126999c15f5a
technillogue <technillogue@gmail.com>
2023-04-14 00:42:53 -0400

    clean up dockerfile, tweak prompt to be a little more concise, and add score+describe apis

--- Dockerfile
+++ Dockerfile
@@ -5,29 +5,20 @@ RUN curl -f https://get.pnpm.io/v6.16.js | node - add --global pnpm
 COPY ./package.json ./pnpm-lock.yaml /app/
 
 FROM pnpm as deps
-RUN pnpm install;
+RUN pnpm install --prod; # we don't want dev deps in release
 
 FROM pnpm as builder
-COPY --from=deps /app/node_modules ./node_modules/
+RUN pnpm install # we need to install dev deps to build
+COPY ./.swcrc /app/
 # enumerate specifically to exclude .git etc 
-COPY ./package.json ./pnpm-lock.yaml ./.swcrc /app/
 COPY ./src /app/src
-# Rebuild the source code only when needed
+# rebuild the source code only when needed
 RUN pnpm run build
 
-# Production image, copy all the files and run next
+# production image, copy all the files
 FROM node:18-alpine AS runner
 WORKDIR /app
 
-RUN addgroup --system --gid 1001 nodejs
-RUN adduser --system --uid 1001 nextjs # eh
-
-COPY --from=deps /app/node_modules ./node_modules/
+COPY --from=deps /app/node_modules ./node_modules/ # prod only
 COPY --from=builder /app/dist/index.js /app/
-
-USER nextjs
-
-EXPOSE 3000
-
-ENV PORT 3000
 ENTRYPOINT ["/usr/local/bin/node", "/app/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,11 @@ You're highly artistic, creative, insightful, an incredible writer and a master 
 
 Rewrite prompts for an image generator that excels at capturing vibes and emotions. Create prompts that are rich in visual language, using modifiers, style descriptors, and artistic choices. Focus on emotion, atmosphere, action, and aesthetics. 
 
-Visual elements: Describe the key visual elements in the scene, including objects, characters, and their properties (size, shape, etc and especially color).
+If the input doesn't seem to be a prompt, if it's a reply to something and doesn't describe an image, crate an image or scene relating to the input in some way, or a prompt that uses words from the input. Be creative and humourous 
 
-Emotion and atmosphere: Use emotive language and adjectives to convey the mood or atmosphere of the scene. This can include aspects like lighting, weather, and the overall emotional tone.
+Visual elements: Describe visual elements in the scene, including objects, characters, and their properties (color, style).
+
+Emotion and atmosphere: emotive language, adjectives to convey the mood or atmosphere of the scene. lighting, weather, overall emotional tone.
 
 Action: Describe any action or movement in the scene in detail
 
@@ -31,16 +33,16 @@ Steps:
 2. Use evocative language to convey the emotion, atmosphere, and action in the scene. Incorporate detailed imagery and style descriptors to enhance the scene. Embrace ambiguity when appropriate, prioritizing the overall vibe and essence of the image.
 3. Write the prompt in the form of alt text for the ideal image.
 
-Remember, the goal is to create prompts that are rich in visual language and evocative, emphasizing the overall vibe, emotion, and artistic qualities of the ideal image. Only respond with the reworded prompt, nothing else. Don't qualify or hedge, output alt text for the ideal image.
-`
+Remember, the goal is to create prompts that are rich in visual language and evocative, emphasizing the overall vibe, emotion, and artistic qualities of the ideal image. Only respond with the reworded prompt, nothing else. Don't qualify or hedge, output alt text for the ideal image.`
 
 async function improve_prompt(prompt: string) {
   const messages: ChatCompletionRequestMessage[] = [
+    // ideally 300 tokens
     { "role": "system", "content": SYSTEM_PROMPT },
     { "role": "user", "content": "Original Prompt: forest" },
     { "role": "assistant", "content": "Ethereal forest, lush verdant foliage, delicate tendrils of dappled sunlight filtering through a canopy of leaves, vibrant flora, serene atmosphere where time stands still, enchanting painting style, essence of nature's beauty with soft watercolor brushstrokes, harmony and tranquility" },
     { "role": "user", "content": "Original Prompt: A futuristic city skyline at night" },
-    { "role": "assistant", "content": "Futuristic city skyline at night, neon lights, futuristic architecture, cyberpunk style.  Breathtaking city skyline, meld of past present and future, kaleidoscopic neon lights intermingle with the soft, warm glow of nostalgic street lamps, luminous sheen upon glassy skyscrapers that pierce the heavens with daring, innovative designs, palpable energy of a cyberpunk metropolis buzzing with life at every corner, bold strokes, vivid colors, dynamic futuristic art style, boundlessly creative, unquenchable thirst for progress" },
+    { "role": "assistant", "content": "Futuristic city skyline at night, neon lights, futuristic architecture, cyberpunk style.  Breathtaking city skyline, meld of past present and future, kaleidoscopic neon lights, soft warm glow of nostalgic street lamps, luminous sheen on glassy skyscrapers that pierce the heavens with daring innovative designs, palpable energy of a cyberpunk metropolis buzzing with life, bold strokes, vivid colors, dynamic futuristic art style, unquenchable thirst for progress" },
     { "role": "user", "content": "Original Prompt: garden with flowers and dna strands" },
     { "role": "assistant", "content": "psychedelic 3d vector art illustration of garden full of colorful double helix dna strands and exotic flowers by lisa frank, beeple and tim hildebrandt, hyper realism, art deco, intricate, elegant, highly detailed, unreal engine, octane render,  smooth, sharp focus, sharp contrast"},
     { "role": "user", "content": "Original Prompt: humanoid plant monster" },
@@ -55,12 +57,39 @@ async function improve_prompt(prompt: string) {
   return completion.data.choices[0].message;
 }
 
+async function get_aesthetic_score(text: string): Promise<number> {
+  const resp = await fetch("https://good-post-detector.fly.dev/good", {
+    method: "POST",
+    body: text,
+  });
+  return parseFloat(await resp.text());
+}
+
+interface ImageDescriptions {
+  descriptions: string[]
+}
+
+async function describe_image(
+  image_url: string,
+  personality: string = "Creative"
+): Promise<ImageDescriptions | object> {
+  const descriptions = await fetch(
+    `http://cliptalk.tenant-dryad-spark.knative.chi.coreweave.com/url?personality=${personality}`,
+    { method: "POST", body: image_url }
+  ).then((r) => r.json());
+  console.log("descriptions: ", descriptions);
+  if (Array.isArray(descriptions) && descriptions.every((d) => typeof d === "string"))
+    return { descriptions: descriptions };
+  return descriptions
+}
+
+
+
 async function generate_prompt(prompt: string) {
   const params = {
     model: "verdant",
     params: { prompts: [{ text: prompt }] },
-    username: "imogen",
-    cost: 0,
+    username: process.env.SPARKL_USERNAME,
   };
   const resp = await fetch("https://oneirograf-prod.fly.dev/prompt", {
     method: "POST",
@@ -78,6 +107,7 @@ async function generate_prompt(prompt: string) {
     await new Promise((r) => setTimeout(r, 1000));
   }
 }
+
 
 async function uploadImage(agent: BskyAgent, url: string): Promise<BlobRef> {
   const response = await fetch(url);
@@ -118,6 +148,14 @@ async function process_notifs(agent: BskyAgent): Promise<void> {
         console.log("no text, skipping");
         continue;
       }
+      // if you're tagged with an empty message in the replies, take the text from the first non-empty tweet
+
+      // if it's an image, describe it, then respond to that
+
+      // later, ideally, adapting system prompts instead of langchain: 
+      // if the conversation calls for generating an image, decide if it's more dreamy or realistic
+      // and use either vqgan or sd to generate an image
+
       const orig_prompt = post_record.text.replace("@imogen.bsky.social", "");
       const improved_prompt = await improve_prompt(orig_prompt);
       let prompt: string;
